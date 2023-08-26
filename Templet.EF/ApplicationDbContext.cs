6d0ea66d479a04ce009f2af8edccf70f3e35273e
Noor Assaf <noorassaf413@gmail.com>
2023-08-26 23:52:54 +0300

    Add project files.

---
+++ Templet.EF/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Templet.Core.Models;
+
+namespace Templet.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+    }
+}
